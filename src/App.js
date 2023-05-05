ac64c4e6a8fd9f31ad90de3cc91e96a3f66d8446
touzimed <meedtouzi28@gmail.com>
2023-05-05 21:34:27 +0200

    hhhh

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import BasicExample from './components/Formulaire'
+
+function App() {
+  return (
+  <div>
+
+    <h1>          
+      Artificial intelligence
+    </h1>
+    <p>the ability of a digital computer or computer-controlled robot 
+    to perform tasks commonly associated with intelligent beings. 
+    The term is frequently applied to the project of developing 
+    systems endowed with the intellectual procevsses characteristic of humans
+    , such as the ability to reason, discover meafning, generalize, or learn from past experience.
+     Since the development of the digital computer in the 1940s, it has been demonstrated that 
+     computers can be programmed to carry out very complex tasks.
+     To make a perfect experTo make a perfect experience with some AI examples, fill out this form and you will receive the link.
+     </p>
+    <BasicExample/>
+
+  </div> 
+ 
+  )
+  
+  
+  
+}
+
+export default App;
